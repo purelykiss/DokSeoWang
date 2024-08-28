6662a4095dc74bcf24369f951cba402eff066d8c
purelykiss <100986618+purelykiss@users.noreply.github.com>
2024-08-28 19:06:27 +0900

    Initial commit

---
+++ src/main/java/kr/co/kopo/dokseowang/DokSeoWangApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kopo.dokseowang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokSeoWangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokSeoWangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/kopo/dokseowang/DokSeoWangApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kopo.dokseowang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokSeoWangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
